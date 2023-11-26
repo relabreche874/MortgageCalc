6c0c30da857b319d5142cb27b9380f4a614d3a50
ubuntu <rlabreche419@gmail.com>
2023-11-26 02:08:38 -0500

    moved scan.close because it caused an error, needed to be in while loop

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args){
         int principal = (int) readNumber("Principal: ", 1000, 1_000_000);
-        float annualInterest = (float) readNumber("Annual Interest Rate: ", 1, 3.92);
+        float annualInterest = (float) readNumber("Annual Interest Rate: ", 1, 4);
         byte years = (byte) readNumber("Period of Mortgage: ", 1, 30);
 
         double mortgage = calculateMortgage(principal, annualInterest, years);
@@ -15,17 +15,19 @@ public class Main {
     }
 
     public static double readNumber(String prompt, double min, double max){
+        // a new prompt will be passed each time the method is called
+        // as well as the min and max values
         Scanner scan = new Scanner(System.in);
         double value;
         while(true) {
             System.out.print(prompt);
             value = scan.nextFloat();
             if(value >= min && value <= max)
-
+                // the values passed to the method call are used here
                 break;
+            scan.close();
             System.out.println("Please enter a value between " + min + " and " + max);
         }
-        scan.close();
         return value;
     }
 
@@ -39,7 +41,7 @@ public class Main {
         float monthlyInterestRate = annualInterest / MONTHS_IN_YEAR / PERCENT;
         short numOfPayments = (short)(years * MONTHS_IN_YEAR);
         double mathPow = Math.pow(1 + monthlyInterestRate, numOfPayments);
-        // did this to simplify the mortgage calculation
+        // did this ^^^  to simplify the mortgage calculation
         double mortgage = principal * ((monthlyInterestRate * mathPow) / (mathPow - 1));
 
         return mortgage;
