97a2e78a146052494927105d0030f2994aea93a6
ubuntu <rlabreche419@gmail.com>
2023-11-26 01:56:00 -0500

    refactored code. removed repetitive while loops, created method to handle each prompt

--- src/Main.java
+++ src/Main.java
@@ -3,46 +3,30 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args){
-
-        Scanner scan= new Scanner(System.in);
-
-        int principal = 0;
-        byte years = 0;
-        float annualInterest = 0;
-
-
-        while(true){
-            System.out.print("Please enter the principal: ");
-            principal = scan.nextInt();
-            if(principal >= 1000 && principal <= 1_000_000)
-                break;
-            System.out.println("Enter a value between 1000 and 1000000");
-        }
-
-        while(true) {
-            System.out.print("Please enter the annual interest rate: ");
-            annualInterest = scan.nextFloat();
-            if(annualInterest >= 1 && annualInterest <= 4.0)
-                break;
-            System.out.println("Please enter a value between 1.0 and 4.0 ");
-        }
-
-        while(true) {
-            System.out.print("Please enter the period of the mortgage: ");
-            years = scan.nextByte();
-            if(years >= 1 && years <=30)
-                // only need a byte for the number of years
-                break;
-            System.out.println("Please enter a value between 1 and 30");
-        }
-
-        scan.close();
+        int principal = (int) readNumber("Principal: ", 1000, 1_000_000);
+        float annualInterest = (float) readNumber("Annual Interest Rate: ", 1, 3.92);
+        byte years = (byte) readNumber("Period of Mortgage: ", 1, 30);
 
         double mortgage = calculateMortgage(principal, annualInterest, years);
 
         String monthlyFormatted = NumberFormat.getCurrencyInstance().format(mortgage);
 
         System.out.print("Mortage: " + monthlyFormatted);
+    }
+
+    public static double readNumber(String prompt, double min, double max){
+        Scanner scan = new Scanner(System.in);
+        double value;
+        while(true) {
+            System.out.print(prompt);
+            value = scan.nextFloat();
+            if(value >= min && value <= max)
+
+                break;
+            System.out.println("Please enter a value between " + min + " and " + max);
+        }
+        scan.close();
+        return value;
     }
 
     public static double calculateMortgage(int principal,
