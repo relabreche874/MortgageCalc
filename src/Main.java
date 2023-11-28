1f02c8713b827cc600d046f73d18c75f49d0121d
ubuntu <rlabreche419@gmail.com>
2023-11-28 17:41:38 -0500

    added calculateMortgage method for mortgage payment schedule

--- src/Main.java
+++ src/Main.java
@@ -2,27 +2,28 @@ import java.text.NumberFormat;
 import java.util.Scanner;
 
 public class Main {
+    final static byte MONTHS_IN_YEAR = 12;
+    final static int PERCENT = 100;
+    // made constants because these values should never change
     public static void main(String[] args){
         int principal = (int) readNumber("Principal: ", 1000, 1_000_000);
-        float annualInterest = (float) readNumber("Annual Interest Rate: ", 1, 4);
+        float annualInterest = (float) readNumber("Annual Interest Rate: ", 1, 5);
         byte years = (byte) readNumber("Period of Mortgage: ", 1, 30);
 
         double mortgage = calculateMortgage(principal, annualInterest, years);
+        String mortgageFormatted = NumberFormat.getCurrencyInstance().format(mortgage);
+        System.out.println("\nMORTGAGE" + "\n--------" + "\nMonthly Payments: " + mortgageFormatted);
 
-        String monthlyFormatted = NumberFormat.getCurrencyInstance().format(mortgage);
-        //move to paymentSchedule when ready
+        System.out.println();
+        System.out.println("PAYMENT SCHEDULE");
+        System.out.println("----------------");
 
-        System.out.print("\nMORTGAGE" + "\n--------" + "\nMonthly Payments: " + monthlyFormatted);
-        //move to paymentSchedule when ready
-    }
+        for(short month = 1; month <= years * MONTHS_IN_YEAR; month++){
+            double balance = calculateBalance(principal, annualInterest, years, month);
+            System.out.println(NumberFormat.getCurrencyInstance().format(balance));
 
-    public static double paymentSchedule(int principal,
-                                         float monthlyInterest,
-                                         byte years){
-        double schedule;
-
-        return 0;
-    }
+            }
+        }
 
     public static double readNumber(String prompt, double min, double max){
         // a new prompt will be passed each time the method is called
@@ -40,12 +41,27 @@ public class Main {
         return value;
     }
 
+    public static double calculateBalance(int principal,
+                                          float annualInterest,
+                                          byte years,
+                                          short numberOfPaymentsMade){
+
+        float monthlyInterestRate = annualInterest / MONTHS_IN_YEAR / PERCENT;
+        short numOfPayments = (short)(years * MONTHS_IN_YEAR);
+
+        double balance = principal *
+                (Math.pow(1 + monthlyInterestRate, numOfPayments) - Math.pow(1 + monthlyInterestRate, numberOfPaymentsMade))
+                / (Math.pow(1 + monthlyInterestRate, numOfPayments) - 1);
+
+        return balance;
+
+    }
+        // schedule = principal ((1 + month_interest)^num_of_payments)- (1 + month_interest)^payments_made) / ((1 + month_interest)^num_of_payments - 1))
+
     public static double calculateMortgage(int principal,
                                            float annualInterest,
                                            byte years){
-        final byte MONTHS_IN_YEAR = 12;
-        final int PERCENT = 100;
-        // made constants because these values should never change
+
 
         float monthlyInterestRate = annualInterest / MONTHS_IN_YEAR / PERCENT;
         short numOfPayments = (short)(years * MONTHS_IN_YEAR);
